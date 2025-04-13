325c8b16b8ef9e417bd5cee224efacf248efd394
viet <phanquocvietspkt1992@gmail.com>
2025-04-13 12:28:30 +0700

    apply flyway

--- src/main/java/com/example/identity_service/IdentityServiceApplication.java
+++ src/main/java/com/example/identity_service/IdentityServiceApplication.java
@@ -2,8 +2,13 @@ package com.example.identity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+@EntityScan("com.example.identity_service.entities")
+@EnableJpaRepositories("com.example.identity_service.repositories")
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
